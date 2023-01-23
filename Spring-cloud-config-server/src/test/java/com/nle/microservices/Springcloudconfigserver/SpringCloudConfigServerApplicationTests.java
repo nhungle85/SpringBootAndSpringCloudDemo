b88dea4a5a0c8b16233436e206dacdfcc00ae21f
Nhung Le <misssg85@gmail.com>
2023-01-23 09:44:16 -0600

    Create spring-cloud-config-server project

---
+++ Spring-cloud-config-server/src/test/java/com/nle/microservices/Springcloudconfigserver/SpringCloudConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nle.microservices.Springcloudconfigserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
